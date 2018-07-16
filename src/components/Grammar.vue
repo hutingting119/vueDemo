470b14caba9b9ab877dd8e7bab2682c9bedb881a
hutingting119 <1912257350@qq.com>
2018-07-16 10:55:47 +0800

    add grammer

---
+++ src/components/Grammar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>{{number+1}}</div>
+    <div>{{ok?'yes':'no'}}</div>
+    <div>{{message.split(' ').reverse().join(' ')}}</div>
+    <div v-if="seen">you can see me</div>
+    <div v-if="!seen">you can't seen me</div>
+    <div><a v-bind:href="url">百度一下</a></div>
+    <button v-on:click="numberAdd()">hi</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        number: 6,
+        ok: true,
+        message: 'this is grammer message',
+        seen: true,
+        url: "https://www.baidu.com",
+      }
+    },
+    methods: {
+      numberAdd:function () {
+        console.log('hello')
+      }
+    }
+  }
+</script>
